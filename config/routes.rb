c1d7aa566aa10f84eb4a55cbae00fa1b3fc3c8fb
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-08-01 10:33:35 +0000

    [Update]bookers2 ルーティング途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  get 'books/new'
+  get 'books/show'
+  get 'books/index'
+  get 'books/edit'
+  
   root to:"homes#top"
+  get 'home/about'=>'homes#about',as:'about'
+  
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
