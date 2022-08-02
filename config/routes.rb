82ed2a4a19abdc3c1b024ea7c1daae97e775fa52
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-08-02 04:09:42 +0000

    [Update]Bookers2　ヘッダー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/show'
-  get 'books/index'
-  get 'books/edit'
-  
+  devise_for :users
   root to:"homes#top"
   get 'home/about'=>'homes#about',as:'about'
   
-  devise_for :users
+  resources:books,only:[:new,:index,:show]
+  resources:users,only:[:index,:show,:edit]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
