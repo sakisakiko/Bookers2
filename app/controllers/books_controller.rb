8b35ccdaf9c329e2dbf5ae38e2a447c1af145742
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-08-03 15:54:58 +0000

    [Update]Bookers2 投稿ページ作成２

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,19 +9,20 @@ class BooksController < ApplicationController
   end
   
   def create
-    @book=Book.new(book_params)
-    @book.user_id=current_user.id
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
     @book.save
-    redirect_to book_path(@book)
+    redirect_to book_path(@book.id)
   end
 
   def edit
   end
   
   private
-  def books
-  params.require(:book).permit(:title,:body)
-  end
+  
+   def post_books
+    params.require(:book).permit(:title, :body)
+   end
   
 end
 
