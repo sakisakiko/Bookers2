d3d149c17c85dfb2a7514e9a4cbaca8b802ecf8b
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-08-04 15:09:49 +0000

    [Update]Bookers2 投稿画面作成３

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,10 +6,11 @@ class BooksController < ApplicationController
   def index
     @book=Book.new
     @books=Book.all
+    @user=current_user
   end
   
   def create
-    @book = Book.new(book_params)
+    @book = Book.new
     @book.user_id = current_user.id
     @book.save
     redirect_to book_path(@book.id)
