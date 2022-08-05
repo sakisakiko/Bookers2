e15e62eda8f0f8b2d47f6361b4a342b722db96e2
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-08-05 11:10:33 +0000

    [Uodate]Bookers2 book 編集ページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,28 +1,46 @@
 class BooksController < ApplicationController
 
   def show
+    @book=Book.find(params[:id])
   end
 
-  def index
-    @books=Book.all
-    @user=current_user
-  end
-  
   def create
-    @book = Book.new
+    @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
     redirect_to book_path(@book.id)
   end
 
-  def edit
+
+  def index
+    @book=Book.new
+    @books=Book.all
+    @user=current_user
   end
-  
+
+
+  def edit
+    @book=Book.find(params[:id])
+  end
+
+  def update
+    @book=Book.find(params[:id])
+    @book.update
+    redirect_to book_path(@book.id)
+  end
+
+  def destroy
+    @book=Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+
   private
-  
-   def post_books
-    params.require(:book).permit(:title, :body)
-   end
-  
+
+  def book_params
+    params.require(:book).permit(:title,:body,)
+  end
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   root to:"homes#top"
   get 'home/about'=>'homes#about',as:'about'
   
-  resources:books,only:[:new,:create,:index,:show,:edit]
+  resources:books,only:[:new,:create,:index,:show,:edit,:update,:destroy]
   resources:users,only:[:index,:show,:edit]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
+
