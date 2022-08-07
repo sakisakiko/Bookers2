a9cd23e49b3bc6dca7543c7f4710b3839946c7e2
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-08-07 04:41:19 +0000

    [Update]Bookers2 バリデーション実装途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,12 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+     redirect_to book_path(@book.id)
+    else
+       @books=Book.all
+       render :index
+    end
   end
 
 
@@ -41,6 +45,7 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title,:body,)
   end
-
+  
+ 
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
     
     has_one_attached:profile_image
     
+   
     def  get_profile_image(width,height)
      unless profile_image.attached?
        file_path=Rails.root.join('app/assets/images/sample-author1.jpg')
