88f0e62cdf4d7cce1ad288e4e2a707eb5f13756c
“sakisakiko” <108504337+sakisakiko@users.noreply.github.com@users.noreply.github.com>
2022-08-07 09:51:03 +0000

    [Update]Bookers2 バリデーション途中２

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,10 +2,12 @@ class Book < ApplicationRecord
 
   has_one_attached :image
   belongs_to :user
+ 
+# titleは空でないこと
+# bodyは空でないかつ最大200文字であること
+   validates:title, presence: true
+   validates:body, presence: true,length: {maximum:200}
   
-   validates:title,presence:true
-   validates:body,presence:true
-  
-
+   
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,12 @@ class User < ApplicationRecord
     
     has_one_attached:profile_image
     
+    # nameは重複しないかつ、２〜２０字まで
+    # introductionは最大５０字まで
+    validates:name,uniqueness: true,length: {minimum: 2, maximum: 20}
+    validates:introduction,length: {maximum: 50}
+  
+
    
     def  get_profile_image(width,height)
      unless profile_image.attached?
